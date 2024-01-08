ffe4de87784fbdbde7052ddd88fffb9547e83844
TheDevSA <ndlovu.code@outlook.com>
2024-01-08 12:42:18 +0200

    changed back to flo

--- app/layout.js
+++ app/layout.js
@@ -13,9 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
-      </head>
       <body className={inter.className}>
       <Navbar/>
         {children}
